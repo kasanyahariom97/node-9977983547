2cda85ff83a7963d3b6fd1c4eadb882e05085ee2
Harry <harry@test.local>
2019-09-17 15:00:00 +0530

    Master Putting Request

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 
 const mongoose = require("mongoose");
 
+const cors = require("cors");
+
 const bodyParser = require("body-parser");
 
 const dbConfig = require('./config/note.config');
+
+app.use(cors());
 
 app.use(express.static('public'));
 
